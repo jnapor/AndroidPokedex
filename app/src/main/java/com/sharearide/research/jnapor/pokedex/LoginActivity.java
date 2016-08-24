1a20e497c1b8b1898ae24075f5e024db634b0c42
jnapor <apor.johnnichol00@gmail.com>
2016-08-24 09:08:31 +0800

    Added Login Functionality

--- app/src/main/java/com/sharearide/research/jnapor/pokedex/LoginActivity.java
+++ app/src/main/java/com/sharearide/research/jnapor/pokedex/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.sharearide.research.jnapor.pokedex;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sharearide.research.jnapor.pokedex.data.DatabaseManipulator;
 import com.sharearide.research.jnapor.pokedex.data.PokedexContract;
@@ -30,10 +34,27 @@ public class LoginActivity extends AppCompatActivity {
                 String pass = password.getText().toString();
 
                 int count = DatabaseManipulator.login(getApplicationContext(), uname, pass);
-                
+                if(count == 0){
+                    showAlertDialog();
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
 
+    private void showAlertDialog(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Incorrect username or password!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 }
